2ab53a796cf91f45df1060de3aeaaaf460aa0028
ruffsl <roxfoxpox@gmail.com>
2020-01-20 00:07:52 -0800

    Use message filters for image transport

--- include/kimera_vio_ros/KimeraVioNode.hpp
+++ include/kimera_vio_ros/KimeraVioNode.hpp
@@ -4,20 +4,40 @@
 #include <string>
 #include <thread>
 
+#include "image_transport/subscriber_filter.h"
+
+#include "message_filters/subscriber.h"
+#include "message_filters/synchronizer.h"
+#include "message_filters/sync_policies/exact_time.h"
+
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+
+#include "sensor_msgs/msg/image.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+
+
+using namespace message_filters::sync_policies;
+using namespace sensor_msgs::msg;
 
 class KimeraVioNode : public rclcpp::Node{
 public:
     KimeraVioNode();
 
 private:
-    void subscriber1_cb(const std_msgs::msg::String::SharedPtr msg);
-    void subscriber2_cb(const std_msgs::msg::String::SharedPtr msg);
+
+    void stereo_cb(
+        const Image::ConstSharedPtr& left,
+        const Image::ConstSharedPtr& right);
+    void imu_cb(const Imu::SharedPtr msg);
     std::string timing_string();
 
-    rclcpp::callback_group::CallbackGroup::SharedPtr callback_group_subscriber1_;
-    rclcpp::callback_group::CallbackGroup::SharedPtr callback_group_subscriber2_;
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription1_;
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription2_;
+    rclcpp::callback_group::CallbackGroup::SharedPtr callback_group_stereo_;
+    rclcpp::callback_group::CallbackGroup::SharedPtr callback_group_imu_;
+    rclcpp::Subscription<Imu>::SharedPtr imu_sub_;
+    
+    typedef ExactTime<Image, Image> ExactPolicy;
+    typedef message_filters::Synchronizer<ExactPolicy> ExactSync;
+    std::shared_ptr<ExactSync> exact_sync_;
+    image_transport::SubscriberFilter left_sub_, right_sub_;
+
 };
--- src/KimeraVioNode.cpp
+++ src/KimeraVioNode.cpp
@@ -15,51 +15,64 @@ std::string KimeraVioNode::timing_string()
 KimeraVioNode::KimeraVioNode()
 : Node("KimeraVioNode")
 {
-  callback_group_subscriber1_ = this->create_callback_group(
+  callback_group_stereo_ = this->create_callback_group(
     rclcpp::callback_group::CallbackGroupType::MutuallyExclusive);
-  callback_group_subscriber2_ = this->create_callback_group(
+  callback_group_imu_ = this->create_callback_group(
     rclcpp::callback_group::CallbackGroupType::MutuallyExclusive);
 
-  auto sub1_opt = rclcpp::SubscriptionOptions();
-  sub1_opt.callback_group = callback_group_subscriber1_;
-  auto sub2_opt = rclcpp::SubscriptionOptions();
-  sub2_opt.callback_group = callback_group_subscriber2_;
+  auto stereo_opt = rclcpp::SubscriptionOptions();
+  stereo_opt.callback_group = callback_group_stereo_;
+  auto imu_opt = rclcpp::SubscriptionOptions();
+  imu_opt.callback_group = callback_group_imu_;
 
-  subscription1_ = this->create_subscription<std_msgs::msg::String>(
+  imu_sub_ = this->create_subscription<Imu>(
     "topic",
     rclcpp::QoS(10),
     std::bind(
-      &KimeraVioNode::subscriber1_cb,
+      &KimeraVioNode::imu_cb,
       this,
       std::placeholders::_1),
-    sub1_opt);
+    imu_opt);
+  
+  std::string transport = "raw";
+  std::string left_topic = "left_cam";
+  std::string right_topic = "right_cam";
+  image_transport::TransportHints hints(this, transport);
 
-  subscription2_ = this->create_subscription<std_msgs::msg::String>(
-    "topic",
-    rclcpp::QoS(10),
-    std::bind(
-      &KimeraVioNode::subscriber2_cb,
-      this,
-      std::placeholders::_1),
-    sub2_opt);
+  left_sub_.subscribe(this, left_topic, hints.getTransport());
+  right_sub_.subscribe(this, right_topic, hints.getTransport());
+
+  int queue_size_ = 10;
+  exact_sync_.reset( new ExactSync(
+    ExactPolicy(queue_size_),
+    left_sub_,
+    right_sub_) );
+   exact_sync_->registerCallback(
+     std::bind(
+       &KimeraVioNode::stereo_cb,
+       this,
+       std::placeholders::_1,
+       std::placeholders::_2));
 }
 
-void KimeraVioNode::subscriber1_cb(const std_msgs::msg::String::SharedPtr msg)
+void KimeraVioNode::stereo_cb(
+  const Image::ConstSharedPtr& left,
+  const Image::ConstSharedPtr& right)
 {
   auto message_received_at = timing_string();
 
   // Extract current thread
   auto thread_string = "THREAD " + string_thread_id();
   thread_string += " => Heard '%s' at " + message_received_at;
-  RCLCPP_INFO(this->get_logger(), thread_string, msg->data.c_str());
+  // RCLCPP_INFO(this->get_logger(), thread_string, msg->data.c_str());
 }
 
-void KimeraVioNode::subscriber2_cb(const std_msgs::msg::String::SharedPtr msg)
+void KimeraVioNode::imu_cb(const Imu::SharedPtr msg)
 {
   auto message_received_at = timing_string();
 
   // Extract current thread
   auto thread_string = "THREAD " + string_thread_id();
   thread_string += " => Heard '%s' at " + message_received_at;
-  RCLCPP_INFO(this->get_logger(), thread_string, msg->data.c_str());
+  // RCLCPP_INFO(this->get_logger(), thread_string, msg->data.c_str());
 }
