07fa9effb7b91d5f8c4d83484a858804e394cde0
ruffsl <roxfoxpox@gmail.com>
2020-02-03 20:53:33 -0800

    Remove ros args before gflag parsing

--- src/KimeraVio.cpp
+++ src/KimeraVio.cpp
@@ -2,16 +2,21 @@
 
 int main(int argc, char * argv[])
 {
+  auto g_args = rclcpp::init_and_remove_ros_arguments(argc, argv);
+  int g_argc = g_args.size();
   // Initialize Google's flags library.
-  google::ParseCommandLineFlags(&argc, &argv, true);
+  google::ParseCommandLineFlags(&g_argc, &argv, true);
   // Initialize Google's logging library.
   google::InitGoogleLogging(argv[0]);
 
-  rclcpp::init(argc, argv);
+//  rclcpp::init(argc, argv);
   rclcpp::executors::MultiThreadedExecutor executor;
+//    rclcpp::executors::SingleThreadedExecutor executor;
   auto kimera_vio_node = std::make_shared<KimeraVioNode>("kimera_node");
   executor.add_node(kimera_vio_node);
   executor.spin();
+
+//  rclcpp::spin(std::make_shared<KimeraVioNode>("kimera_node"));
   rclcpp::shutdown();
   return 0;
 }
