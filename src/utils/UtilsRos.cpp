bade61e8ffc6a6b40d4ebbc96f0587fa3dc6ccc8
Toni <tonirosinol@hotmail.com>
2020-08-02 21:31:37 -0400

    Fix issue #96

--- src/utils/UtilsRos.cpp
+++ src/utils/UtilsRos.cpp
@@ -60,9 +60,6 @@ void msgCamInfoToCameraParams(const sensor_msgs::CameraInfoConstPtr& cam_info,
   CHECK(!cam_params->camera_id_.empty());
 
   cam_params->distortion_model_ = cam_info->distortion_model;
-  CHECK(cam_params->distortion_model_ == "radtan" ||
-        cam_params->distortion_model_ == "radial-tangential" ||
-        cam_params->distortion_model_ == "equidistant");
 
   const std::vector<double>& distortion_coeffs = cam_info->D;
   CHECK_EQ(distortion_coeffs.size(), 4);
