6fab64db1c9aee675286c8931f84cb587dd47540
Yulun Tian <yulun@mit.edu>
2020-10-09 17:30:10 -0400

    Update logger node to save ground truth trajectory from DCIST simulator

--- scripts/gt_logger_node.py
+++ scripts/gt_logger_node.py
@@ -4,6 +4,7 @@ import os
 import csv
 
 import rospy
+from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import TransformStamped
 from nav_msgs.msg import Odometry
 
@@ -15,8 +16,38 @@ class GTLoggerNode:
         self.output_csv_file = os.path.join(self.output_dir, "traj_gt.csv")
 
         # rospy.Subscriber(self.gt_topic, TransformStamped, self.gt_cb_tsf)
-        rospy.Subscriber(self.gt_topic, Odometry, self.gt_cb_odom, queue_size=20)
+        # rospy.Subscriber(self.gt_topic, Odometry, self.gt_cb_odom, queue_size=20)
+        rospy.Subscriber(self.gt_topic, PoseStamped, self.gt_cb_pose, queue_size=20)
         self.setup_gt_file()
+
+    def gt_cb_pose(self, msg):
+        """ Callback for ground-truth poses as they come in as PoseStamped
+            msgs.
+
+            Writes each pose to a csv file in the swe format. This format is
+            as follows:
+                timestamp[ns], x, y, z, qw, qx, qy, qz
+            x, y, z are position coordinates in the world frame and qx-qw are
+            quaternion values, also in the world frame. Everything afterwards
+            is zero.
+
+            Args:
+                // TODO: this should be a odometry msg...
+                msg: A geometry_msgs/PoseStamped object representing
+                     the current ground-truth body transform.
+        """
+        with open(self.output_csv_file, mode='a') as file:
+            # Because csv only accepts one-char delimiters, we add the space
+            # in each datum.
+            writer = csv.writer(file, delimiter=",")
+            writer.writerow([str(msg.header.stamp.to_nsec()),
+                             str(msg.pose.position.x),
+                             str(msg.pose.position.y),
+                             str(msg.pose.position.z),
+                             str(msg.pose.orientation.w),
+                             str(msg.pose.orientation.x),
+                             str(msg.pose.orientation.y),
+                             str(msg.pose.orientation.z)])
 
     def gt_cb_tfs(self, msg):
         """ Callback for ground-truth poses as they come in as TransformStamped
